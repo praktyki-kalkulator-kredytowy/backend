ad6fea5db375d87c33c41e5f811518aee373f846
markopolo139 <marekseget@onet.pl>
2021-05-04 15:24:34 +0200

    fix(controller): add pathVariable to concatenation method

--- src/main/java/com/praktyki/backend/web/controllers/StringController.java
+++ src/main/java/com/praktyki/backend/web/controllers/StringController.java
@@ -4,6 +4,7 @@ import com.praktyki.backend.interactors.StringConcatenationUseCase;
 import com.praktyki.backend.services.exception.EntityNotFound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +13,8 @@ public class StringController {
     @Autowired
     private StringConcatenationUseCase mStringConcatenationUseCase;
 
-    @GetMapping("/api/v1/concat")
-    public String concatenation(String text) throws EntityNotFound {
+    @GetMapping("/api/v1/concat/{strValue}")
+    public String concatenation(@PathVariable(name = "strValue") String text) throws EntityNotFound {
         return mStringConcatenationUseCase.concatenation(text);
     }
 
