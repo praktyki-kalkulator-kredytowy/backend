cd3f1bdd87e189620aad243cf62e911d8d1d9129
xsw2-2wsx <jan1undrych@gmail.com>
2021-05-05 08:54:05 +0200

    feat(ErrorHandler): add logs

--- src/main/java/com/praktyki/backend/web/error/ErrorHandler.java
+++ src/main/java/com/praktyki/backend/web/error/ErrorHandler.java
@@ -1,6 +1,8 @@
 package com.praktyki.backend.web.error;
 
 import com.praktyki.backend.services.exception.EntityNotFoundException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,15 +11,29 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 @ControllerAdvice
 public class ErrorHandler extends ResponseEntityExceptionHandler {
+
+    private static Logger log = LogManager.getLogger();
 
     @Autowired
     private HttpServletRequest mRequest;
 
     private ResponseEntity<Object> createResponseEntity(ApiError error) {
         return new ResponseEntity<Object>(error, error.getStatus());
+    }
+
+    private String stackTraceToString(Throwable t) {
+        StringWriter writer = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(writer);
+
+        t.printStackTrace(printWriter);
+        printWriter.flush();
+
+        return writer.toString();
     }
 
 
@@ -37,11 +53,10 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<Object> handleException(Exception ex) {
         ApiError error = ApiError.builder().build();
 
-        ex.printStackTrace();
+        log.error(stackTraceToString(ex));
 
         return createResponseEntity(error);
     }
-
 
 
 }
