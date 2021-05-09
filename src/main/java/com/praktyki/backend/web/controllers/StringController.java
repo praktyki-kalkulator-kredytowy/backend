33c0afe2e6dcacf71fac08c426ad8ddf4707d979
markopolo139 <marekseget@onet.pl>
2021-05-09 21:40:10 +0200

    refractor(StringController): Change validation annottation place

--- src/main/java/com/praktyki/backend/web/controllers/StringController.java
+++ src/main/java/com/praktyki/backend/web/controllers/StringController.java
@@ -3,21 +3,22 @@ package com.praktyki.backend.web.controllers;
 import com.praktyki.backend.interactors.StringConcatenationUseCase;
 import com.praktyki.backend.services.exception.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 
 @CrossOrigin
+@Validated
 @RestController
 public class StringController {
 
     @Autowired
     private StringConcatenationUseCase mStringConcatenationUseCase;
 
-    @GetMapping("/api/v1/concat/")
+    @GetMapping("/api/v1/concat")
     public String concatenation(
-            @Valid
             @NotBlank(message = "Please specify a string")
             @RequestParam(name = "value") String text
     ) throws EntityNotFoundException {
