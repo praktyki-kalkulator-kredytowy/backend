3d13586ff0bbdcae03a03cf1938782acfc298fa4
markopolo139 <marekseget@onet.pl>
2021-05-11 10:14:14 +0200

    feat(sevices): create InsuranceService

---
+++ src/main/java/com/praktyki/backend/business/services/InsuranceService.java
@@ -0,0 +1,21 @@
+package com.praktyki.backend.business.services;
+
+import com.praktyki.backend.business.value.InsurancePremium;
+import com.praktyki.backend.business.value.ScheduleConfiguration;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class InsuranceService {
+    public List<InsurancePremium> calculateInsurancePremium(ScheduleConfiguration scheduleConfiguration) {
+
+        return null;
+
+    }
+
+    public BigDecimal sumUpInsurancePremium(List<InsurancePremium> insurancePremium) {
+        return insurancePremium.stream()
+                .map(InsurancePremium::getInsurancePremiumValue)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+}
