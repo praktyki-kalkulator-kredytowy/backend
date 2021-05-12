b97eeaf84df32fef2f89354d6570c3005c6722ef
markopolo139 <marekseget@onet.pl>
2021-05-12 10:27:14 +0200

    feat(shcemas): create schema for credit calculator

--- schemas/db-schema.sql
+++ schemas/db-schema.sql
@@ -2,3 +2,22 @@ create table strings(
     strings_id int not null primary key,
     `value` varchar(100) not null
 );
+create table configuration_groups(
+    groups_id int not null primary key auto_increment,
+    group_name varchar(100) not null
+);
+create table configuration_values(
+    values_id int not null primary key auto_increment,
+    `key` varchar(150) not null,
+    `value` varchar(100) not null
+);
+create table groups_to_values(
+    groups_id int not null,
+    values_id int not null,
+    key(groups_id),
+    key(values_id),
+    constraint connect_groups_id foreign key (groups_id) references configuration_groups(groups_id),
+    constraint connect_values_id foreign key (values_id) references configuration_values(values_id)
+);
+
+
