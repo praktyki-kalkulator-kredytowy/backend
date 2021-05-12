3d251f250c386909bc685f2ae6270e4bfdab2ed9
xsw2-2wsx <jan1undrych@gmail.com>
2021-05-12 14:53:45 +0200

    temp

--- schemas/db-schema.sql
+++ schemas/db-schema.sql
@@ -2,22 +2,22 @@ create table strings(
     strings_id int not null primary key auto_increment,
     `value` varchar(100) not null
 );
+
 create table configuration_groups(
-    groups_id int not null primary key auto_increment,
-    group_name varchar(100) not null
+    configuration_groups_id int not null primary key auto_increment,
+    name varchar(100) not null,
+    key (name)
 );
+
 create table configuration_values(
     values_id int not null primary key auto_increment,
+    configuration_groups_id int not null,
     `key` varchar(150) not null,
-    `value` varchar(100) not null
-);
-create table groups_to_values(
-    groups_id int not null,
-    values_id int not null,
-    key(groups_id),
-    key(values_id),
-    constraint connect_groups_id foreign key (groups_id) references configuration_groups(groups_id),
-    constraint connect_values_id foreign key (values_id) references configuration_values(values_id)
+    `value` varchar(100) not null,
+    key (configuration_groups_id),
+    constraint configuration_groups_id_to_configuration_groups
+    foreign key (configuration_groups_id)
+    references configuration_groups(configuration_groups_id)
 );
 
 
