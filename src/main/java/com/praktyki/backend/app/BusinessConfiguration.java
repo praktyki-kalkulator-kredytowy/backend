98956ba37fbd15566778c203a330a0b411f32d00
xsw2-2wsx <jan1undrych@gmail.com>
2021-05-13 11:54:46 +0200

    feat(BusinessConfiguration): create a bean for APRCService

--- src/main/java/com/praktyki/backend/app/BusinessConfiguration.java
+++ src/main/java/com/praktyki/backend/app/BusinessConfiguration.java
@@ -1,6 +1,7 @@
 package com.praktyki.backend.app;
 
 import com.praktyki.backend.business.entities.dates.QuarterlyDateScheduleCalculator;
+import com.praktyki.backend.business.services.APRCService;
 import com.praktyki.backend.business.services.InsuranceService;
 import com.praktyki.backend.business.services.StringService;
 import com.praktyki.backend.business.services.InstallmentScheduleService;
@@ -40,5 +41,11 @@ public class BusinessConfiguration {
     @Scope("singleton")
     public InsuranceService getInsuranceService(QuarterlyDateScheduleCalculator calculator) {
         return new InsuranceService(calculator);
+    }
+
+    @Bean
+    @Scope("singleton")
+    public APRCService getAPRCService() {
+        return new APRCService();
     }
 }
