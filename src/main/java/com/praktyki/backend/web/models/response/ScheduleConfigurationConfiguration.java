f93c3ce17e911467e67687617e1f9371f284ecc6
xsw2-2wsx <jan1undrych@gmail.com>
2021-05-21 09:47:02 +0200

    feat(ScheduleConfConf): add info required for validation

--- src/main/java/com/praktyki/backend/web/models/response/ScheduleConfigurationConfiguration.java
+++ src/main/java/com/praktyki/backend/web/models/response/ScheduleConfigurationConfiguration.java
@@ -6,8 +6,18 @@ import com.praktyki.backend.configuration.Configuration;
 public class ScheduleConfigurationConfiguration {
 
     public double defaultCommissionRate;
+    public double minInterestRate;
+    public double maxInterestRate;
+    public double minCommissionRate;
+    public double maxCommissionRate;
+
 
     public ScheduleConfigurationConfiguration(Configuration configuration) {
         this.defaultCommissionRate = Double.parseDouble(configuration.get(ConfigurationKeys.DEFAULT_COMMISSION_RATE));
+        this.minInterestRate = Double.parseDouble(configuration.get(ConfigurationKeys.MIN_INTEREST_RATE));
+        this.maxInterestRate = Double.parseDouble(configuration.get(ConfigurationKeys.MAX_INTEREST_RATE));
+        this.minCommissionRate = Double.parseDouble(configuration.get(ConfigurationKeys.MIN_COMMISSION_RATE));
+        this.maxCommissionRate = Double.parseDouble(configuration.get(ConfigurationKeys.MAX_COMMISSION_RATE));
+
     }
 }
