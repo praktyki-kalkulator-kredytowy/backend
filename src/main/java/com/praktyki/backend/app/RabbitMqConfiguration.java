e4f6083f4088ed4b4e23156fcf2b5bf3ce230620
xsw2-2wsx <jan1undrych@gmail.com>
2021-05-26 13:06:44 +0200

    fix(RabbitMQConfiguration): create ConnectionFctory bean

--- src/main/java/com/praktyki/backend/app/RabbitMqConfiguration.java
+++ src/main/java/com/praktyki/backend/app/RabbitMqConfiguration.java
@@ -1,6 +1,8 @@
 package com.praktyki.backend.app;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
@@ -11,6 +13,11 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMqConfiguration {
 
     public static String EXCHANGE_NAME = "schedules";
+
+    @Bean
+    public CachingConnectionFactory connectionFactory() {
+        return new CachingConnectionFactory("localhost");
+    }
 
     @Bean
     public FanoutExchange getFanOut() {
@@ -33,7 +40,9 @@ public class RabbitMqConfiguration {
     }
 
 
-    public AmqpTemplate getAmqpTemplate(RabbitTemplate template) {
+    @Bean
+    public AmqpTemplate getAmqpTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
         template.setMessageConverter(getJsonMassageConverter());
         return template;
     }
